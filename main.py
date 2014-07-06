89c618c8eb3c49ad825ac28884a8ed0fcde3a9ea
Kirik snegovick Konstantin <snegovick@gmail.com>
2014-07-07 01:45:18 +0400

    added centralized main window class to store all widgets

--- main.py
+++ main.py
@@ -45,12 +45,9 @@ class Screen(gtk.DrawingArea):
     # Handle the expose-event by drawing
     def do_expose_event(self, event):
         # Create the cairo context
-        
-        
+
         cr_gdk = self.window.cairo_create()
-
         surface = cr_gdk.get_target()
-
         cr_surf = cairo.ImageSurface(cairo.FORMAT_ARGB32, self.allocation.width, self.allocation.height)
         cr = cairo.Context(cr_surf)
         
@@ -69,81 +66,88 @@ class Screen(gtk.DrawingArea):
         cr_gdk.set_source_surface(cr_surf)
         cr_gdk.paint()
 
-def __mk_labeled_spin(mlabel, callback, value=3.0, lower=0, upper=999.0, step_incr=0.01, page_incr=0.5):
-    hbox = gtk.HBox(homogeneous=False, spacing=0)
-    label = gtk.Label(mlabel)
-    spin = gtk.SpinButton(adjustment=gtk.Adjustment(value=value, lower=lower, upper=upper, step_incr=step_incr, page_incr=page_incr, page_size=0), climb_rate=0.01, digits=3)
-    #alignment = gtk.Alignment(1.0, 0.0, 1.0, 0.0)
-    #alignment.add(spin)
-    hbox.pack_start(label, expand=False, fill=False, padding=0)
-    hbox.pack_start(spin, expand=True, fill=True, padding=0)
-    return hbox
+class MainWindow(object):
+    def __init__(self):
+        self.tool_diameter_spin = {}
+        self.tool_feedrate_spin = {}
+        self.tool_vert_step = {}
 
-def __mk_right_vbox():
-    right_vbox = gtk.VBox(homogeneous=False, spacing=0)
+        self.__mk_left_vbox()
 
-    tool_label = gtk.Label("Tool settings")
-    right_vbox.pack_start(tool_label, expand=False, fill=False, padding=0)
+        self.hbox = gtk.HBox(homogeneous=False, spacing=0)
+        self.hbox.pack_start(self.left_vbox, expand=False, fill=False, padding=0)
+        self.hbox.pack_start(self.widget, expand=True, fill=True, padding=0)
 
-    tool_diameter_hbox = __mk_labeled_spin("Diameter, mm: ", None)
-    right_vbox.pack_start(tool_diameter_hbox, expand=False, fill=False, padding=0)
+        self.__mk_right_vbox()
+        self.hbox.pack_start(self.right_vbox, expand=False, fill=False, padding=0)
+        self.window = gtk.Window()
+        self.window.resize(width, height)
+        self.window.connect("delete-event", gtk.main_quit)
+        self.widget = Widget()
+        self.widget.connect("button_press_event", self.widget.button_press_event)
+        self.widget.set_events(gtk.gdk.BUTTON_PRESS_MASK)
+        #widget.window = window
+        
+        #widget.paths = paths
 
-    tool_feedrate_hbox = __mk_labeled_spin("Feedrate, mm/s: ", None)
-    right_vbox.pack_start(tool_feedrate_hbox, expand=False, fill=False, padding=0)
+        gobject.timeout_add(10, self.widget.periodic)
+        self.window.add(self.hbox)
+        self.window.show_all()
+        self.window.present()
+        gtk.main()
 
-    tool_vertical_step_hbox = __mk_labeled_spin("Vertical step, mm: ", None)
-    right_vbox.pack_start(tool_vertical_step_hbox, expand=False, fill=False, padding=0)
+
+    def __mk_labeled_spin(self, dct, mlabel, callback, value=3.0, lower=0, upper=999.0, step_incr=0.01, page_incr=0.5):
+        hbox = gtk.HBox(homogeneous=False, spacing=0)
+        dct["hbox"] = hbox
+        label = gtk.Label(mlabel)
+        dct["label"] = label
+        spin = gtk.SpinButton(adjustment=gtk.Adjustment(value=value, lower=lower, upper=upper, step_incr=step_incr, page_incr=page_incr, page_size=0), climb_rate=0.01, digits=3)
+        dct["spin"] = spin
+        #alignment = gtk.Alignment(1.0, 0.0, 1.0, 0.0)
+        #alignment.add(spin)
+        hbox.pack_start(label, expand=False, fill=False, padding=0)
+        hbox.pack_start(spin, expand=True, fill=True, padding=0)
+        return hbox
+
+    def __mk_right_vbox(self):
+        self.right_vbox = gtk.VBox(homogeneous=False, spacing=0)
+
+        self.tool_label = gtk.Label("Tool settings")
+        self.right_vbox.pack_start(self.tool_label, expand=False, fill=False, padding=0)
+
+        self.tool_diameter_hbox = self.__mk_labeled_spin(self.tool_diameter_spin, "Diameter, mm: ", None)
+        self.right_vbox.pack_start(self.tool_diameter_hbox, expand=False, fill=False, padding=0)
+
+        self.tool_feedrate_hbox = self.__mk_labeled_spin(self.tool_feedrate_spin, "Feedrate, mm/s: ", None)
+        self.right_vbox.pack_start(self.tool_feedrate_hbox, expand=False, fill=False, padding=0)
+
+        self.tool_vertical_step_hbox = self.__mk_labeled_spin(self.tool_vert_step, "Vertical step, mm: ", None)
+        self.right_vbox.pack_start(self.tool_vertical_step_hbox, expand=False, fill=False, padding=0)
 
 
-    tool_label = gtk.Label("Contour following settings")
-    right_vbox.pack_start(tool_label, expand=False, fill=False, padding=0)
+        self.tool_label = gtk.Label("Contour following settings")
+        self.right_vbox.pack_start(self.tool_label, expand=False, fill=False, padding=0)
 
-    tool_label = gtk.Label("Pocketing settings")
-    right_vbox.pack_start(tool_label, expand=False, fill=False, padding=0)
+        self.tool_label = gtk.Label("Pocketing settings")
+        self.right_vbox.pack_start(self.tool_label, expand=False, fill=False, padding=0)
 
-    return right_vbox
+    def __mk_left_vbox(self):
+        self.left_vbox = gtk.VBox(homogeneous=False, spacing=0)
+        self.load_dxf = gtk.Button(label="Load...")
+        self.load_dxf.connect("clicked", lambda *args: ep.push_event(ee.load_click, args))
+        self.left_vbox.pack_start(self.load_dxf, expand=False, fill=False, padding=0)
 
-def __mk_left_vbox():
-    left_vbox = gtk.VBox(homogeneous=False, spacing=0)
-    load_dxf = gtk.Button(label="Load...")
-    load_dxf.connect("clicked", lambda *args: ep.push_event(ee.load_click, args))
-    left_vbox.pack_start(load_dxf, expand=False, fill=False, padding=0)
-
-    paths_label = gtk.Label("Paths")
-    scrolled_window = gtk.ScrolledWindow()
-    gtklist = gtk.List()
-    scrolled_window.add_with_viewport(gtklist)
-    left_vbox.pack_start(paths_label, expand=False, fill=False, padding=0)
-    left_vbox.pack_start(scrolled_window, expand=True, fill=True, padding=0)
-
-    return left_vbox
+        self.paths_label = gtk.Label("Paths")
+        self.scrolled_window = gtk.ScrolledWindow()
+        self.gtklist = gtk.List()
+        self.scrolled_window.add_with_viewport(self.gtklist)
+        self.left_vbox.pack_start(self.paths_label, expand=False, fill=False, padding=0)
+        self.left_vbox.pack_start(self.scrolled_window, expand=True, fill=True, padding=0)
         
 # GTK mumbo-jumbo to show the widget in a window and quit when it's closed
 def run(Widget):
-    window = gtk.Window()
-    window.resize(width, height)
-    window.connect("delete-event", gtk.main_quit)
-    widget = Widget()
-    widget.connect("button_press_event", widget.button_press_event)
-    widget.set_events(gtk.gdk.BUTTON_PRESS_MASK)
-    #widget.window = window
-
-    #widget.paths = paths
-
-    left_vbox = __mk_left_vbox()
-
-    hbox = gtk.HBox(homogeneous=False, spacing=0)
-    hbox.pack_start(left_vbox, expand=False, fill=False, padding=0)
-    hbox.pack_start(widget, expand=True, fill=True, padding=0)
-
-    right_vbox = __mk_right_vbox()
-    hbox.pack_start(right_vbox, expand=False, fill=False, padding=0)
-
-    gobject.timeout_add(10, widget.periodic)
-    window.add(hbox)
-    window.show_all()
-    window.present()
-    gtk.main()
+    m = MainWindow()
 
 if __name__ == "__main__":
     run(Screen)
