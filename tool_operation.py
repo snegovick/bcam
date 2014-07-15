02104f5156bfce90479aae65529286a523675686
Kirik snegovick Konstantin <snegovick@gmail.com>
2014-07-16 00:52:20 +0400

    unsaved deletion

--- tool_operation.py
+++ tool_operation.py
@@ -4,19 +4,6 @@ class TOEnum:
     drill = "drill"
     exact_follow = "exact follow"
 
-class TOSetting:
-    def __init__(self, type, min, max, default, display_name, parent_cb):
-        self.type = type
-        self.min = min
-        self.max = max
-        self.default = default
-        self.display_name = display_name
-        self.new_value = None
-        self.parent_callback = parent_cb
-
-    def set_value(self, v):
-        self.new_value = v
-        self.parent_callback(self)
 
 class ToolOperation(object):
     def __init__(self, settings_dispatcher):
