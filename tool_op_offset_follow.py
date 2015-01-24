b7659ef8eb3ed8841072002d914474c9e1442d70
Kirik snegovick Konstantin <snegovick@gmail.com>
2015-01-24 14:50:09 +0300

    Issue: #110
      * Removed old faulty offset-by-scaling method

--- tool_op_offset_follow.py
+++ tool_op_offset_follow.py
@@ -22,20 +22,18 @@ class TOOffsetFollow(TOAbstractFollow):
             self.offset = 0
             self.path = None
             self.offset_path = None
-            self.scale_center = [0, 0]
         else:
             self.deserialize(data)
 
         self.display_name = TOEnum.offset_follow+" "+str(self.index)
 
     def serialize(self):
-        return {'type': 'tooffsetfollow', 'path_ref': self.path.name, 'depth': self.depth, 'index': self.index, 'offset': self.offset, 'scale_center': self.scale_center}
+        return {'type': 'tooffsetfollow', 'path_ref': self.path.name, 'depth': self.depth, 'index': self.index, 'offset': self.offset}
 
     def deserialize(self, data):
         self.depth = data["depth"]
         self.index = data["index"]
         self.offset = data["offset"]
-        self.scale_center = data["scale_center"]
 
         p = self.try_load_path_by_name(data["path_ref"], self.state)
         if p:
@@ -43,9 +41,7 @@ class TOOffsetFollow(TOAbstractFollow):
 
     def get_settings_list(self):
         settings_lst = [TOSetting("float", 0, self.state.settings.material.thickness, self.depth, "Depth, mm: ", self.set_depth_s),
-                        TOSetting("float", None, None, self.offset, "Offset, mm: ", self.set_offset_s),
-                        TOSetting("float", None, None, 1.0, "Scale center x: ", self.set_scale_center_x_s),
-                        TOSetting("float", None, None, 1.0, "Scale center y: ", self.set_scale_center_y_s)]
+                        TOSetting("float", None, None, self.offset, "Offset, mm: ", self.set_offset_s)]
         return settings_lst
 
     def set_depth_s(self, setting):
@@ -53,16 +49,6 @@ class TOOffsetFollow(TOAbstractFollow):
 
     def set_offset_s(self, setting):
         self.offset = setting.new_value
-        self.__build_offset_path(self.path)
-        self.draw_list = self.offset_path
-
-    def set_scale_center_x_s(self, setting):
-        self.scale_center[0] = setting.new_value
-        self.__build_offset_path(self.path)
-        self.draw_list = self.offset_path
-
-    def set_scale_center_y_s(self, setting):
-        self.scale_center[1] = setting.new_value
         self.__build_offset_path(self.path)
         self.draw_list = self.offset_path
 
@@ -205,62 +191,6 @@ class TOOffsetFollow(TOAbstractFollow):
         self.offset_path = new_elements
         debug("  offset_path: "+str(self.offset_path))
 
-
-    def __build_offset_path_scale(self, p):
-        dbgfname()
-        new_elements = []
-        elements = p.get_ordered_elements()
-        if len(elements)==0:
-            return False
-        if len(elements)==1:
-            e = elements[0]
-            if type(e).__name__ == "ECircle":
-                new_elements.append(ECircle(e.center, e.radius+self.offset, e.lt, None))
-            else:
-                return
-        else:
-            # calculate center
-            x_min, x_max = 0, 0
-            y_min, y_max = 0, 0
-            # for i, e in enumerate(elements):
-            #     s = e.start
-            #     e = e.end
-            #     if s[0]>x_max:
-            #         x_max = s[0]
-            #     elif s[0]<x_min:
-            #         x_min = s[0]
-                    
-            #     if s[1]>y_max:
-            #         y_max = s[1]
-            #     elif s[1]<y_min:
-            #         y_min = s[1]
-
-            # cx = (x_max+x_min)/2.0
-            # cy = (y_max+y_min)/2.0
-            center = self.scale_center
-            negative_center = [-center[0], -center[1]]
-
-            for i, e in enumerate(elements):
-                start = e.start
-                start_center_rel = mk_vect(center, start)
-                end = e.end
-                end_center_rel = mk_vect(center, end)
-                
-                scaled_start_center_rel = scale_vect(start_center_rel, self.offset)
-                scaled_end_center_rel = scale_vect(end_center_rel, self.offset)
-                start_zero_rel = mk_vect(negative_center, scaled_start_center_rel)
-                end_zero_rel = mk_vect(negative_center, scaled_end_center_rel)
-
-                if type(e).__name__ == "ELine":
-                    ne = ELine(start_zero_rel, end_zero_rel, e.lt)
-                elif type(e).__name__ == "EArc":
-                    ne = EArc(center=e.center, lt=e.lt, start=start_zero_rel, end=end_zero_rel)
-
-                new_elements.append(ne)
-
-        self.offset_path = new_elements
-        debug("  offset_path: "+str(self.offset_path))
-        
     def apply(self, path):
         dbgfname()
         debug("  apply path: "+str(path))
@@ -277,4 +207,4 @@ class TOOffsetFollow(TOAbstractFollow):
         return self.get_gcode_base(self.path.ordered_elements)
 
     def __repr__(self):
-        return "<Exact follow>"
+        return "<Offset follow>"
