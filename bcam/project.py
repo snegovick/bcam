df040865e498800d283ca39de1e37368e4c5906e
snegovick <snegovick@gmail.com>
2015-02-18 19:35:51 +0400

    Issue: #64
      * Removed prior states from save file

--- bcam/project.py
+++ bcam/project.py
@@ -36,18 +36,17 @@ class Project(object):
         dbgfname()
         from events import ep, ee
         #global state
+
         debug("  loading project from "+str(project_path))
         f = open(project_path)
         data = f.read()
         f.close()
         parsed_json = json.loads(data)
+        self.steps = []
         #print "json:", parsed_json
-        if parsed_json["format_version"] == 1:
-            for s in parsed_json["steps"]:
-                step = Step(data=s)
-                self.steps.append(step)
-
-            debug("  steps: "+str(len(self.steps)))
+        if parsed_json["format_version"] == 2:
+            step = Step(data=parsed_json["step"])
+            self.steps.append(step)
             Singleton.state.set(self.steps[-1].state)
             ep.push_event(ee.update_tool_operations_list, (None))
             ep.push_event(ee.update_paths_list, (None))
@@ -58,16 +57,18 @@ class Project(object):
     def push_state(self, state):
         dbgfname()
         self.steps.append(Step(state))
+        depth = 50
+        if (len(self.steps)>depth):
+            self.steps = self.steps[-depth:]
         debug("  steps length:"+str(len(self.steps)))
 
     def save(self, project_path):
         if os.path.splitext(project_path)[1][1:].strip() != "bcam":
             project_path+=".bcam"
 
-        # format 1
-        serialized_steps = [s.serialize() for s in self.steps]
+        # format 2
         f = open(project_path, 'w')
-        f.write(json.dumps({'format_version': 1, 'steps': serialized_steps}))
+        f.write(json.dumps({'format_version': 2, 'step': self.steps[-1].serialize()}))
         f.close()
         return True
 
